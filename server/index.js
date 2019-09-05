d74b43124523dcf2fb54c280baded4f99ea12227
Wesley <wtschmidt94@gmail.com>
2019-09-05 16:37:35 -0500

    moved server.js info into index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const PORT = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+// const passport = require('passport');//for User authentication
+// const flash = require('connect-flash');//for User authentication pop up notifications
+
 const app = express();
 const path = require('path');
 
+app.use(bodyParser.json());
 app.listen(8080, () => {
-    console.log('Bitches be crazy');
+    console.log('Bitches be crazy on: 8080');
 });
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
