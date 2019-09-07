4b20265cf46ae61694c451101dfb3fd6c01fe9e1
Carin Chapman <Chapman.carin@gmail.com>
2019-09-07 14:48:06 -0500

    Fix syntax errors in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,12 @@
+DROP DATABASE IF EXISTS trashPanda;
+
 CREATE DATABASE trashPanda;
 
 USE trashPanda;
 
 
 CREATE TABLE users (
-  userId int NOT NULL AUTO_INCREMENT,
+  userId INT NOT NULL AUTO_INCREMENT,
   username varchar(50) NOT NULL,
   password varchar(25) NOT NULL,
   email varchar(50) NOT NULL,
@@ -13,14 +15,14 @@ CREATE TABLE users (
 );
 
 CREATE TABLE posts (
-  postId int NOT NULL AUTO_INCREMENT,
-  -- the text field below might need to be <text> instead of <varchar> depending on how long we want posts to be
-  text varchar NOT NULL (255), 
-  img1 varchar NOT NULL (255),
+  postId INT NOT NULL AUTO_INCREMENT,
+  postText varchar (255), 
+  img1 varchar (255) NOT NULL,
   img2 varchar(255),
   img3 varchar(255),
+  userId INT,
   PRIMARY KEY (postId),
-  FOREIGN KEY (user_id) REFERENCES users(userId)
+  FOREIGN KEY (userId) REFERENCES users(userId)
 );
 
 /*  Execute this file from the command line by typing:
