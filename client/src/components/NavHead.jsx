1045ad0f4283b1fb3903bc2a036d32f045ef1c95
Wesley <wtschmidt94@gmail.com>
2019-09-07 12:30:57 -0500

    added POST button

--- client/src/components/NavHead.jsx
+++ client/src/components/NavHead.jsx
@@ -11,13 +11,19 @@ import { Collapse,
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-    Col
+    Col,
+    Button
     } from 'reactstrap';
 
 const NavHead = (props) => {
     return (
             <Navbar backgroundColor="#00CC00">
                 <NavbarBrand href="/">Trash Panda</NavbarBrand>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Button>POST</Button>
+                    </NavItem>
+                </Nav>
             </Navbar>
     );
 }
