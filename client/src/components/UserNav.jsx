c14f204fdd8ecc65d4c83592164bb53219210be9
Wesley <wtschmidt94@gmail.com>
2019-09-07 13:45:54 -0500

    made login and sign-up links, need to expand

--- client/src/components/UserNav.jsx
+++ client/src/components/UserNav.jsx
@@ -7,10 +7,8 @@ const UserNav = (props) => {
             <section float="left" width="10%" margin="0 1.5%">
             <p>USERNAME</p>
             <Nav vertical>
-                <NavLink href="#">Link</NavLink> 
-                <NavLink href="#">Link</NavLink> 
-                <NavLink href="#">Another Link</NavLink> 
-                <NavLink disabled href="#">Disabled Link</NavLink>
+                <NavLink href="#">Login</NavLink> 
+                <NavLink href="#">Sign-up</NavLink>
             </Nav>
             </section>
         </div>
