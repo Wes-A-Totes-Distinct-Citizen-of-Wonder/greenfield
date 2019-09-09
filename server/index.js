8b085a5ecb1aee8d1422f3560ab9c477b59b7f2f
Carin Chapman <Chapman.carin@gmail.com>
2019-09-09 12:18:08 -0500

    Add barebones of what happens after savePost() is invoked

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { saveUser, savePost } = require('./database/index.js')
 
 const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
@@ -8,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const path = require('path');
+const { saveUser, savePost } = require('./database/index.js');
 
 // app.use(express.static(path.join(__dirname, '../client/images')));
 app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -18,6 +18,9 @@ app.post('/submitPost', (req, res) => {
   // need to authenticate user's credentials here.
   // if not logged in, re-route to sign-up page
   // then somehow pull their username out of the req.body, and use that in savePost() call below
+
+  // TEMPORARY standin for userId. replace with actual data when it exists
+  const { userId } = verifySession;
   const post = {
     text: req.body.text,
     img1: req.body.img1,
@@ -25,8 +28,69 @@ app.post('/submitPost', (req, res) => {
     img3: req.body.img3 || null,
     userId,
   };
-  savePost(post);
+
+  savePost(post)
+    .then((savedPost) => {
+      const { userId } = savedPost; // probably not correct but have to see the data to know what this will be in the body
+      
+    });
 });
+
+
+// app.post('/poll', (req, res) => {
+//   let pollId;
+//   let voteId;
+//   // console.log(req.body); an obj, w/props pollTitle, option1, option2, option3,
+//   // and I think bodyParser is parsing it into what I want, but not sure
+//   // save poll to db
+//   savePoll(req.body)
+//     .then((results) => {
+//       console.log(results);
+//       pollId = results.insertId;
+//       // res.status(201).send('got your poll!');
+//     // })
+//     // .catch((error) => {
+//     //   console.log(error);
+//     //   // res.status(404).send('something went wrong');
+//     })
+//     .then(() => {
+//       saveVotes(req.body)
+//         .then((results) => {
+//           console.log(results);
+//           voteId = results.insertId;
+//           // res.status(201).send('got your poll!');
+//         })
+//         // .catch((error) => {
+//         //   console.log(error);
+//         //   // res.status(404).send('something went wrong');
+//         // });
+//       // })
+//         .then(() => {
+//           saveVotesAndPolls(pollId, voteId)
+//             .then(() => {
+//               console.log(pollId, voteId);
+//               res.status(201).send('got your poll!');
+//             })
+//             .catch((error) => {
+//               console.log(error);
+//               res.status(404).send('something went wrong');
+//             });
+//         });
+//     });
+// });
+
+// app.get('/seePolls', (req, res) => {
+//   getPolls()
+//     .then((result) => {
+//       console.log(result);
+//       res.send(result);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+
+
 app.listen(PORT, () => {
   console.log('Bitches be crazy on: 8080');
 });
