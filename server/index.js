456c56ce70b49525d0ae5a8369a1d510654c02a2
Carin Chapman <Chapman.carin@gmail.com>
2019-09-09 11:13:49 -0500

    Require savePost function from db/index.js and write out the bones of an app.post request for the submit Post button

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { saveUser, savePost } = require('./database/index.js')
 
 const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
@@ -13,6 +14,19 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(bodyParser.json());
 
+app.post('/submitPost', (req, res) => {
+  // need to authenticate user's credentials here.
+  // if not logged in, re-route to sign-up page
+  // then somehow pull their username out of the req.body, and use that in savePost() call below
+  const post = {
+    text: req.body.text,
+    img1: req.body.img1,
+    img2: req.body.img2 || null,
+    img3: req.body.img3 || null,
+    userId,
+  };
+  savePost(post);
+});
 app.listen(PORT, () => {
   console.log('Bitches be crazy on: 8080');
 });
