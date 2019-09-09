7fec8f98e2ed915270207c21aab023f96a7fe875
Wesley <wtschmidt94@gmail.com>
2019-09-09 14:58:10 -0500

    changed USERNAME color to white

--- client/src/components/UserNav.jsx
+++ client/src/components/UserNav.jsx
@@ -17,7 +17,7 @@ class UserNav extends React.Component {
     return (
         <div>
             <section float="left" width="10%" margin="0 1.5%">
-            <p>USERNAME</p>
+            <p style={{color: 'white'}}>USERNAME</p>
             <Nav vertical>
                 <NavLink className="Login" href="#">Login</NavLink> 
                 <NavLink className="Sign-up"href="#">Sign-up</NavLink>
