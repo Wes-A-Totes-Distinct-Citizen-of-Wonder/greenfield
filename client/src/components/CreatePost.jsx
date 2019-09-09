5005a5b63a885aeafa2a4f799a1e63d0587388d0
Wesley <wtschmidt94@gmail.com>
2019-09-09 16:11:42 -0500

    added onChange event listener for inputs

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -11,7 +11,7 @@ class CreatePost extends React.Component {
             img: [],
             description: '',
             location: {
-                street: '',
+                address: '',
                 city: '',
                 state: '',
                 zip: '',
@@ -21,28 +21,28 @@ class CreatePost extends React.Component {
                 },
             }
         };
-        this.makePost = this.makePost.bind(this);
+        this.onPostSubmit = this.onPostSubmit.bind(this);
     }
 
-    makePost() {
+    onPostSubmit() {
         // check if user is logged in, if so do this vvvv
-        console.log(post)
+        alert(JSON.stringify(this.state, null, " "))
         /// else prompt user to login/ sign-up
     }
 
     render() {
         return (
-            <Form onSubmit={this.makePost}>
+            <Form onSubmit={this.onPostSubmit}>
                 <FormGroup>
                     <Label for="post-img" style={{ color: 'white' }}>Image File</Label>
-                    <Input type="file" name="file" id="post-img" style={{ color: 'white' }} value={this.state.img} />
+                    <Input type="file" name="file" id="post-img" style={{ color: 'white' }} value={this.state.img} onChange={e => this.setState({ img: e.target.value })}/>
                     <FormText color="muted">
                         Please include an image(s) of the materials you wish to share.
                     </FormText>
                 </FormGroup>
                 <FormGroup>
                     <Label for="post-desc" style={{ color: 'white' }}>Description</Label>
-                    <Input type="textarea" name="text" id="post-desc" value={this.state.description} placeholder="limit of 255 characters" />
+                    <Input type="textarea" name="text" id="post-desc" value={this.state} onChange={e => this.setState({ description: e.target.value })}placeholder="limit of 255 characters" />
                 </FormGroup>
                 <FormGroup check>
                     <Label check for="post-tags" style={{ color: 'white' }}>
@@ -62,17 +62,17 @@ class CreatePost extends React.Component {
                     </Label>
                     <FormText color="muted">
                         Please select at least one related tag.
-                    </FormText>
+                    </FormText>f
                 </FormGroup>
                 <h1 />
                 <FormGroup>
                     <Label for="post-loc" style={{ color: 'white' }}>Location</Label>
-                    <Input type="text" name="text" id="address" placeholder="address" />
+                    <Input type="text" name="text" id="address" placeholder="address" value={this.state.location.address} onChange={e => this.setState.location({ address: e.target.value })}/>
                 </FormGroup>
                 <Row>
-                    <Col sm='6'><Input type="text" name="city" id="city" placeholder="city" /></Col>
-                    <Col sm='4'><Input type="text" name="text" id="state" placeholder="state" /></Col>
-                    <Col sm='2'><Input type="text" name="text" id="zip-code" placeholder="zip" /></Col>
+                    <Col sm='6'><Input type="text" name="city" id="city" placeholder="city" value={this.state.location.city} onChange={e => this.setState.location({ city: e.target.value })}/></Col>
+                    <Col sm='4'><Input type="text" name="text" id="state" placeholder="state" value={this.state.location.state} onChange={e => this.setState.location({ state: e.target.value })}/></Col>
+                    <Col sm='2'><Input type="text" name="text" id="zip-code" placeholder="zip" value={this.state.location.zip} onChange={e => this.setState({ zip: e.target.value })}/></Col>
                 </Row>
                 <hr />
                 <FormGroup>
