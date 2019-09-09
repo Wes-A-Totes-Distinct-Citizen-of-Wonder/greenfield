e60a9cbc4789d03e8cab2ee4b3997d176e60d175
Wesley <wtschmidt94@gmail.com>
2019-09-09 10:29:05 -0500

    changed to stateful

--- client/src/components/UserNav.jsx
+++ client/src/components/UserNav.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
 
-const UserNav = (props) => {
-    return (
-        <div>
-            <section float="left" width="10%" margin="0 1.5%">
-            <p>USERNAME</p>
-            <Nav vertical>
-                <NavLink href="#">Login</NavLink> 
-                <NavLink href="#">Sign-up</NavLink>
-            </Nav>
-            </section>
-        </div>
-    );
+class UserNav extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+
+        }
+
+        userLogin(username, password) {
+
+        }
+        return (
+            <div>
+                <section float="left" width="10%" margin="0 1.5%">
+                <p>USERNAME</p>
+                <Nav vertical>
+                    <NavLink className="Login" href="#">Login</NavLink> 
+                    <NavLink className="Sign-up"href="#">Sign-up</NavLink>
+                </Nav>
+                </section>
+            </div>
+        );
+    }
+    
 }
 
 export default UserNav;
