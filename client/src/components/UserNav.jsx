320512c38920e245a307ac4b7a76b63481c134e1
Wesley <wtschmidt94@gmail.com>
2019-09-10 14:21:39 -0500

    reformatted userNav

--- client/src/components/UserNav.jsx
+++ client/src/components/UserNav.jsx
@@ -14,13 +14,14 @@ const UserNav = (props) => {
         //     // }
         // render() {
     const { changeView } = props;
+    const { user } = props;
     return (
         <div>
             <section float="left" width="10%" margin="0 1.5%">
-            <p style={{color: 'white'}}>USERNAME</p>
             <Nav vertical>
-                <NavLink className="Login" href="#" onClick={() => changeView('login')}>Login</NavLink> 
-                <NavLink className="Sign-up"href="#" onClick={() => changeView('sign-up')}>Sign-up</NavLink>
+                <NavLink className="Username" href="#" style={{color: 'white'}} onClick={() => changeView('user-profile')}><h2>{user.username || ""}</h2></NavLink>
+                <NavLink className="Login" href="#" style={{color: 'white'}} onClick={() => changeView('login')}>Login</NavLink> 
+                <NavLink className="Sign-up" href="#" style={{color: 'white'}} onClick={() => changeView('sign-up')}>Sign-up</NavLink>
             </Nav>
             </section>
         </div>
