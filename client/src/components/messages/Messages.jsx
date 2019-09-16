b382b7d6cb7af03fee5e0f7f6f353098af745a37
gmarinojr <garymarinojr@gmail.com>
2019-09-16 16:33:43 -0500

    created Messages component

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -2,16 +2,42 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 class Messages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-  <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>;
-
-    )
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Subject
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Message
+        <textarea>Type your message here</textarea>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
+
+export default Messages;
