1d68d4f2c1fbddf811d256caa6f4b70a7979f369
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-17 11:46:09 -0500

    added zipcode search input, formatted,

--- client/src/components/SearchTag.jsx
+++ client/src/components/SearchTag.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from 'axios';
 
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Input, InputGroupAddon, InputGroup, Button, Col, Row } from 'reactstrap';
 import Axios from "axios";
+
+
 
 class SearchTag extends React.Component {
     constructor(props) {
@@ -12,6 +14,8 @@ class SearchTag extends React.Component {
             open: false,
         };
         this.toggle = this.toggle.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
     // controls the drop down menu on tags
     toggle() {
@@ -19,22 +23,41 @@ class SearchTag extends React.Component {
             open: !lastState.open
         }))
     }
+    
+    handleClick(){
+      console.log('hey');
+    }
+
+    handleChange(){
+
+    }
 
     render() {
         return (
             <div>
-            <Dropdown isOpen={this.state.open} toggle={this.toggle}>
-                <DropdownToggle caret>
-                    Search by:
-                </DropdownToggle>
-                <DropdownMenu>
+              <Row form>
+
+              <Col md={2}>
+                  <InputGroup>
+                    <Input placeholder='Zipcode'/>
+                    <InputGroupAddon addonType="append"><Button onClick={this.handleClick}>Search</Button></InputGroupAddon>
+                  </InputGroup>
+              </Col>
+
+              <Col sm={2}>
+                <Dropdown isOpen={this.state.open} toggle={this.toggle}>
+                  <DropdownToggle caret>Search by Material</DropdownToggle>
+                  <DropdownMenu>
                     <DropdownItem onClick={() => this.props.searchByTag('lumber')}>Lumber</DropdownItem>
                     <DropdownItem onClick={() => this.props.searchByTag('metal')}>Metal</DropdownItem>
                     <DropdownItem onClick={() => this.props.searchByTag('concrete')}>Concrete</DropdownItem>
                     <DropdownItem onClick={() => this.props.searchByTag('piping')}>Piping</DropdownItem>
                     <DropdownItem onClick={() => this.props.searchByTag('glass')}>Glass</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
+                  </DropdownMenu>
+                </Dropdown>
+              </Col>
+              
+              </Row>
             </div>
         )
     }
