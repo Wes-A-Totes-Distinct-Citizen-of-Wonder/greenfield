2beda36b75c789eaf1c35f482b386095e4e00fd2
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-18 17:24:55 -0500

    added bind statements

--- client/src/components/MyPosts.jsx
+++ client/src/components/MyPosts.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Axios from 'axios';
-import { DropdownToggle, DropdownMenu, DropdownItem, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Col, Row, CardColumns } from 'reactstrap';
+import axios from 'axios';
+// import { DropdownToggle, DropdownMenu, DropdownItem, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Col, Row, CardColumns } from 'reactstrap';
 
 class MyPosts extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      user: '',
 
     };
+    this.getUser = this.getUser.bind(this);
 
-    this.setMyPosts = this.getMyPosts.bind(this);
+ 
     
   }
 
   componentDidMount() {
-    this.setMyPosts();
+    this.getUser();
+
   }
 
-  setMyPosts() {
-    const { getNearbyPosts} = this.props;
-    this.setState({posts: getNearbyPosts });
-    console.log(getNearbyPosts);
+  getMyPosts() {
+    axios.get('/myposts', (req, res) => {
+
+    });
+
+  }
+
+  getUser(){
+    const { currUser } = this.props;
+    console.log(currUser, 'user');
+  
+
   }
 
   render(){
     return (
-      <Card>
-
-      </Card>
+      <div><p>hey</p></div>
     );
   }
 }
