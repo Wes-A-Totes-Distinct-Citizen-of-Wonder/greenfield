291b6cbb976401afa8c982c51ccedf20ec0238a8
May <may0000ff@gmail.com>
2019-09-18 18:25:14 -0500

    moved logo to left and home button next to post button

--- client/src/components/NavHead.jsx
+++ client/src/components/NavHead.jsx
@@ -15,10 +15,10 @@ const NavHead = (props) => {
     const { changeView } = props;
     return (
       <Navbar backgroundcolor="#00CC00">
-        <NavbarBrand href="#" style={{ color: "white" }} onClick={() => { changeView('home') }}>Home</NavbarBrand>
-        <img src={require('./../../images/reConstructIcon.png')} width='20%' />
+        <img src={require('./../../images/reConstructIcon.png')} href="#" onClick={() => { changeView('home') }} width='20%' />
         <Nav navbar>
           <NavItem>
+            <Button href="#" onClick={() => { changeView('home') }}>Home</Button>
             <Button onClick={() => { changeView('create-post') }}>Post</Button>
           </NavItem>
         </Nav>
