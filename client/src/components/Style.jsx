0af9b63186fda87214a07575c0b3b9fab67ec877
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-19 12:15:59 -0500

    added styles

--- client/src/components/Style.jsx
+++ client/src/components/Style.jsx
@@ -1,11 +1,29 @@
-
 const imgPreview = {
   height: '100px',
   width: '150px',
   overflow: 'hidden',
   float: 'left',
+  margin: '2px',
+};
+
+const white = {
+  color: '#FFFFFF',
+};
+
+const label = {
+  color: '#FFFFFF',
+};
+
+const pageHeader = {
+  background: 'rgb(147, 174, 194)',
+  color: '#FFFFFF',
+  'border-radius': '5px',
+  'text-align': 'center',
 };
 
 module.exports = {
   imgPreview,
-}
+  white,
+  label,
+  pageHeader,
+};
