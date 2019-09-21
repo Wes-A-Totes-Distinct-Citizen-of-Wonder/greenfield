40ec0889d3763d27f45d6c1411c09326b07bc57a
gmarinojr <garymarinojr@gmail.com>
2019-09-21 17:41:55 -0500

    selectedMessage pops up

--- client/src/components/messages/SelectMessage.jsx
+++ client/src/components/messages/SelectMessage.jsx
@@ -1,47 +1,29 @@
 import React from 'react';
-import axios from 'axios';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Card, CardBody, CardHeader, CardTitle } from 'reactstrap';
 
-class SelectMessage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+// class SelectMessage extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
       
-    };
-  }
+//     };
+//   }
+const SelectMessage = (props) => {
+  const { messages } = props;
 
-  render() {
     return (
       <div>
-        <Form onSubmit={this.onMessageSubmit}>
-          <FormGroup>
-            <Label style={{ color: 'white' }}>Subject</Label>
-            <Input
-              type='text'
-              name='text'
-              id='subject'
-              value={this.state.subject}
-              onChange={e => this.setState({ subject: e.target.value })}
-              placeholder='subject'></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for='post-desc' style={{ color: 'white' }}>
-              Description
-            </Label>
-            <Input
-              type='textarea'
-              name='text'
-              id='content'
-              value={this.state.content}
-              onChange={e => this.setState({ content: e.target.value })}
-              placeholder='message'
-            />
-          </FormGroup>
-          <Button color='primary'>Send</Button>{' '}
-        </Form>
+        <Card>
+          <CardHeader>{messages.subject}</CardHeader>
+          <CardBody>
+            <CardTitle>{messages.content}</CardTitle>
+            <Button>Reply</Button>
+          </CardBody>
+        </Card>
       </div>
     );
   }
-}
+
+
 
 export default SelectMessage;
